08bef9bb63b2cded7ef0648bfa9f720de5e0f101
Naima A.C <kheyrrenimco@gmail.com>
2026-09-23 23:21:21 +0300

    Assigment_1

---
+++ Assigment1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Assigment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblstudentid = new System.Windows.Forms.Label();
+            this.lbldepartment = new System.Windows.Forms.Label();
+            this.lblsemester = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtstudentid = new System.Windows.Forms.TextBox();
+            this.txtdepartment = new System.Windows.Forms.TextBox();
+            this.txtsemester = new System.Windows.Forms.TextBox();
+            this.btnshowinfo = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(61, 32);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(177, 20);
+            this.lblname.TabIndex = 0;
+            this.lblname.Text = "Enter the student name\r\n";
+            // 
+            // lblstudentid
+            // 
+            this.lblstudentid.AutoSize = true;
+            this.lblstudentid.Location = new System.Drawing.Point(61, 74);
+            this.lblstudentid.Name = "lblstudentid";
+            this.lblstudentid.Size = new System.Drawing.Size(154, 20);
+            this.lblstudentid.TabIndex = 0;
+            this.lblstudentid.Text = "Enter the student ID\r\n";
+            // 
+            // lbldepartment
+            // 
+            this.lbldepartment.AutoSize = true;
+            this.lbldepartment.Location = new System.Drawing.Point(61, 110);
+            this.lbldepartment.Name = "lbldepartment";
+            this.lbldepartment.Size = new System.Drawing.Size(161, 20);
+            this.lbldepartment.TabIndex = 0;
+            this.lbldepartment.Text = "Enter the department\r\n";
+            // 
+            // lblsemester
+            // 
+            this.lblsemester.AutoSize = true;
+            this.lblsemester.Location = new System.Drawing.Point(61, 149);
+            this.lblsemester.Name = "lblsemester";
+            this.lblsemester.Size = new System.Drawing.Size(145, 20);
+            this.lblsemester.TabIndex = 0;
+            this.lblsemester.Text = "Enter the semester\r\n";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(284, 32);
+            this.txtname.Multiline = true;
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(274, 26);
+            this.txtname.TabIndex = 1;
+            // 
+            // txtstudentid
+            // 
+            this.txtstudentid.Location = new System.Drawing.Point(282, 74);
+            this.txtstudentid.Multiline = true;
+            this.txtstudentid.Name = "txtstudentid";
+            this.txtstudentid.Size = new System.Drawing.Size(274, 26);
+            this.txtstudentid.TabIndex = 1;
+            // 
+            // txtdepartment
+            // 
+            this.txtdepartment.Location = new System.Drawing.Point(275, 110);
+            this.txtdepartment.Multiline = true;
+            this.txtdepartment.Name = "txtdepartment";
+            this.txtdepartment.Size = new System.Drawing.Size(274, 26);
+            this.txtdepartment.TabIndex = 1;
+            // 
+            // txtsemester
+            // 
+            this.txtsemester.Location = new System.Drawing.Point(275, 149);
+            this.txtsemester.Multiline = true;
+            this.txtsemester.Name = "txtsemester";
+            this.txtsemester.Size = new System.Drawing.Size(274, 26);
+            this.txtsemester.TabIndex = 1;
+            // 
+            // btnshowinfo
+            // 
+            this.btnshowinfo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnshowinfo.Location = new System.Drawing.Point(97, 306);
+            this.btnshowinfo.Name = "btnshowinfo";
+            this.btnshowinfo.Size = new System.Drawing.Size(141, 42);
+            this.btnshowinfo.TabIndex = 3;
+            this.btnshowinfo.Text = "show information";
+            this.btnshowinfo.UseVisualStyleBackColor = false;
+            this.btnshowinfo.Click += new System.EventHandler(this.btnshowinfo_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnclear.Location = new System.Drawing.Point(314, 306);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(141, 42);
+            this.btnclear.TabIndex = 3;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = false;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbloutput.Location = new System.Drawing.Point(63, 203);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(493, 84);
+            this.lbloutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1179, 570);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnshowinfo);
+            this.Controls.Add(this.txtsemester);
+            this.Controls.Add(this.txtdepartment);
+            this.Controls.Add(this.txtstudentid);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lblsemester);
+            this.Controls.Add(this.lbldepartment);
+            this.Controls.Add(this.lblstudentid);
+            this.Controls.Add(this.lblname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblstudentid;
+        private System.Windows.Forms.Label lbldepartment;
+        private System.Windows.Forms.Label lblsemester;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtstudentid;
+        private System.Windows.Forms.TextBox txtdepartment;
+        private System.Windows.Forms.TextBox txtsemester;
+        private System.Windows.Forms.Button btnshowinfo;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Label lbloutput;
+    }
+}
+
---
+++ Assigment1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assigment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnshowinfo_Click(object sender, EventArgs e)
+        {
+
+            //stage 1of input
+            //creating variable
+
+            string name,department,fulldata;
+            int studentid;
+            int semester;
+
+            //initial values to variable
+            name = txtname.Text;
+            studentid = int.Parse(txtstudentid.Text);
+            department = txtdepartment.Text;
+            semester = int.Parse(txtsemester.Text);
+
+            //stage 2 process - comcatination of fulldata
+            
+            fulldata= name + " " +studentid + " " +department + " " +semester;
+
+            //stage 3 output using lable
+            lbloutput.Text = fulldata;
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            //cleaning texbox and label
+            txtname.Clear();
+            txtstudentid.Clear();
+            txtdepartment.Clear();
+            txtsemester.Clear();
+            lbloutput.Text = " ";
+        }
+    }
+}
